02dda763aab421cbe06f5f432d2c7fec17adad0f
Tuomas Savolainen <tuomas.developer@gmail.com>
2015-07-18 22:16:41 +0300

    added some images and clickable box

---
+++ Assets/ClickableBox.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableBox : MonoBehaviour {
+	
+	// Happens when clicked
+	void OnMouseDown(){
+		Debug.Log ("clicked BaseStar");
+	}
+	
+}
